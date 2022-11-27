7cbd68827dab5b689e95bfe07fdc77d8f77b54b8
wyoung163 <20201014@sungshin.ac.kr>
2022-11-27 23:14:32 +0900

    chore: add database file to connect with MySQL

---
+++ db.js
@@ -0,0 +1,15 @@
+const dotenv = require("dotenv");
+const mysql = require("mysql2/promise");
+
+dotenv.config();
+
+const pool = mysql.createPool({
+  host     : process.env.DB_HOST,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PWD,
+  database : process.env.DB_NAME,
+  dateStrings: 'date',
+  charset: 'utf8mb4',
+});
+
+module.exports = pool;
