1255b8d6caa9e9adf188ea2bc7ba8135a835f956
snoWYoung <80168342+wyoung163@users.noreply.github.com>
2022-12-15 22:15:18 +0900

    docs: add host IP

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const mysql = require("mysql2/promise");
 dotenv.config();
 
 const pool = mysql.createPool({
-  host     : process.env.DB_HOST,
+  host     : process.env.DB_HOST || "34.64.210.53",
   user     : process.env.DB_USER,
   password : process.env.DB_PWD,
   database : process.env.DB_NAME,
