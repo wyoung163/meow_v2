787dfc47a4d4d5fdb477a199033bd521893431e8
snoWYoung <80168342+wyoung163@users.noreply.github.com>
2022-12-15 20:33:16 +0900

    docs: add double quotation marks

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const mysql = require("mysql2/promise");
 dotenv.config();
 
 const pool = mysql.createPool({
-  host     : process.env.DB_HOST || 34.64.210.53,
+  host     : process.env.DB_HOST || "34.64.210.53",
   user     : process.env.DB_USER || "wyoung",
   password : process.env.DB_PWD || "wyoung",
   database : process.env.DB_NAME || "meow",
