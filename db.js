ae1b3371e692aeeed01384a08347a8d352632c58
snoWYoung <80168342+wyoung163@users.noreply.github.com>
2022-12-15 20:36:42 +0900

    docs: delete password

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ dotenv.config();
 const pool = mysql.createPool({
   host     : process.env.DB_HOST || "34.64.210.53",
   user     : process.env.DB_USER || "wyoung",
-  password : process.env.DB_PWD || "wyoung",
+  password : process.env.DB_PWD,
   database : process.env.DB_NAME || "meow",
   dateStrings: 'date',
   charset: 'utf8mb4',
